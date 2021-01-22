23f875eeae363ca098dcd3c0111901e9b17fd172
ghostfaced02 <73943968+ghostfaced02@users.noreply.github.com>
2021-01-22 22:01:09 +0600

    Update 0.2
    -Added player
    -Added player movement
    -Added Camera movement
    -Added Package BayatGames
    -Added Mitosis

---
+++ Assets/Mitosis.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mitosis : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject cellPrefab;
+
+    public float shootingForce = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ApplyMitosis();
+        }
+
+
+    }
+    void ApplyMitosis()
+    {
+        GameObject cell = Instantiate(cellPrefab, firepoint.position, firepoint.rotation);
+        Rigidbody2D rb = cell.GetComponent<Rigidbody2D>();
+        rb.AddForce(firepoint.up * shootingForce, ForceMode2D.Impulse);
+    }
+}
